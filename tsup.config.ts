a5b79dfbd2501471393ea6b1910db2cd7e83fd38
frolleks <frolleks@hotmail.com>
2023-06-23 15:02:10 +0700

    BREAKING CHANGE: move to typescript

---
+++ tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["index.ts", "lib/**/*.ts"],
+  format: ["esm", "cjs"],
+  ignoreWatch: ["**/dist", "**/node_modules", "**/.DS_STORE", "**/.git"],
+  dts: true,
+  splitting: false,
+  sourcemap: false,
+  clean: true,
+});
